ecbe614eb0af3465e12fdb6b730e2971d4a3b4cd
ismailmellai <ismail.mellai@mail.com>
2016-02-18 21:59:02 +0000

    Partie modèle et vue : Done

--- model.js
+++ model.js
@@ -1,3 +1,42 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+
+function Drawing() {
+ this.formes= [];
+}
+
+
+function Shape(color,size){
+this.color=color;
+this.size=size;
+}
+
+
+function Ligne(x1,y1,x2,y2,size,color){
+
+Shape.call(this,size,color);
+
+this.x1=x1;
+this.y1=y1;
+this.x2=x2;
+this.y2=y2;
+}
+
+
+function Rectangle(width,height,x1,y1,size,color){
+
+Shape.call(this,size,color);
+
+this.width=width;
+this.height=height;
+this.x1=x1;
+this.y1=y1;
+}
+
+Ligne.prototype = new Shape;
+Ligne.prototype.constructor = Ligne; 
+Rectangle.prototype = new Shape;
+Rectangle.prototype.constructor = Rectangle; 
+
--- view.js
+++ view.js
@@ -1,2 +1,30 @@
 
-// Implémenter ici les fonctions paint à ajouter dans chacune des classes du modèle.
+
+Rectangle.prototype.paint = function(ctx) {
+ //TODO Manager color
+ ctx.strokeStyle = this.color;
+ ctx.lineWidth = this.size;
+ ctx.rect(this.width, this.height,this.x1, this.y1);
+ ctx.stroke();
+};
+
+Ligne.prototype.paint = function(ctx) {
+ //TODO Manager color
+ ctx.strokeStyle = this.color;
+ ctx.lineWidth = this.size;
+ ctx.beginPath();
+ ctx.moveTo(this.x1, this.y1);
+ ctx.lineTo(this.x2, this.y2);
+ ctx.stroke();
+
+};
+
+Drawing.prototype.paint = function(ctx) {
+ console.log(this.getForms());
+ ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+ ctx.fillRect(0, 0, canvas.width, canvas.height);
+ this.getForms().forEach(function(eltDuTableau) {
+ // now fill the canvas
+ eltDuTableau.paint(ctx);
+ });
+};
